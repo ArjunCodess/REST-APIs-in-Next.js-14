aaeb4489eff47c75c134dccf1f45afd436a7c646
Arjun Vijay Prakash <arjunv.prakash12345@gmail.com>
2024-06-09 17:13:18 +0530

    /blogs DELETE method created.

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -75,12 +75,12 @@ export const PATCH = async (request: Request, context: { params: any }) => {
      }
 
      catch (error: any) {
-          return new NextResponse("Error :: /api/(dashboard)/categories/[category]/route.ts/PATCH : " + error.message, { status: 500 });
+          return new NextResponse("Error :: /api/(dashboard)/blogs/[blog]/route.ts/PATCH : " + error.message, { status: 500 });
      }
 }
 
 export const DELETE = async (request: Request, context: { params: any }) => {
-     const categoryId = context.params.category;
+     const blogId = context.params.blog;
 
      try {
           const { searchParams } = new URL(request.url);
@@ -88,7 +88,7 @@ export const DELETE = async (request: Request, context: { params: any }) => {
 
           if (!userId || !Types.ObjectId.isValid(userId)) return new NextResponse(JSON.stringify({ message: "Invalid or missing userId" }), { status: 400 });
 
-          if (!categoryId || !Types.ObjectId.isValid(categoryId)) return new NextResponse(JSON.stringify({ message: "Invalid or missing categoryId" }), { status: 400 });
+          if (!blogId || !Types.ObjectId.isValid(blogId)) return new NextResponse(JSON.stringify({ message: "Invalid or missing categoryId" }), { status: 400 });
 
           await connectToDB();
 
@@ -96,16 +96,16 @@ export const DELETE = async (request: Request, context: { params: any }) => {
 
           if (!user) return new NextResponse(JSON.stringify({ message: "User not found" }), { status: 400 });
 
-          const category = await Category.findOne({ _id: categoryId, user: userId });
+          const blog = await Blog.findOne({ _id: blogId, user: userId });
 
-          if (!category) return new NextResponse(JSON.stringify({ message: "Category not found" }), { status: 400 });
+          if (!blog) return new NextResponse(JSON.stringify({ message: "Blog not found" }), { status: 400 });
 
-          const deletedCategory = await Category.findByIdAndDelete(categoryId);
+          const deletedBlog = await Blog.findByIdAndDelete(blogId);
 
-          return new NextResponse(JSON.stringify({ message: "Category deleted", user: deletedCategory }), { status: 200 });
+          return new NextResponse(JSON.stringify({ message: "Blog deleted", user: deletedBlog }), { status: 200 });
      }
 
      catch (error: any) {
-          return new NextResponse("Error :: /api/(dashboard)/categories/[category]/route.ts/DELETE : " + error.message, { status: 500 });
+          return new NextResponse("Error :: /api/(dashboard)/blogs/[blog]/route.ts/DELETE : " + error.message, { status: 500 });
      }
 }
